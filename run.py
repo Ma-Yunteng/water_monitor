dad09120eddb4fbf6009b3797e966e53b6cc256c
Jack <git@jackhiggins.ie>
2018-04-28 14:03:04 +0200

    Add shebang to run and make configFile required arg

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 import argparse
 from metermonitor import Config, Viewer, NullViewer, Camera
 
 # Read command line args
 parser = argparse.ArgumentParser(description='Monitor a water meter')
 
-parser.add_argument('-configFile', help='Path to configuration file for this meter')
+parser.add_argument('configFile', help='Path to configuration file for this meter')
 parser.add_argument('--modes', default='LIVE', help='Comma-separated modes to run this program in. May include DEBUG, CALIBRATE')
 args = parser.parse_args()
 
